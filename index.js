0692301ae0bba318198e0fe40f096a7a0a30838e
getlarge <ed@getlarge.eu>
2020-04-26 18:07:37 +0200

    quick config fix

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ require('dotenv').config();
 if (require.main === module) {
   const config = {
     rest: {
-      port: +(process.env.HTTP_SERVER_PORT ?? 3001),
-      host: process.env.HTTP_SERVER_HOST ?? 'localhost',
+      port: +(process.env.HTTP_SERVER_PORT
+        ? process.env.HTTP_SERVER_PORT
+        : 3001),
+      host: process.env.HTTP_SERVER_HOST
+        ? process.env.HTTP_SERVER_HOST
+        : 'localhost',
       gracePeriodForClose: 5000,
       expressSettings: {
         'x-powered-by': false,
@@ -40,12 +44,18 @@ if (require.main === module) {
       },
     },
     ws: {
-      port: +(process.env.WS_SERVER_PORT ?? 3002),
-      host: process.env.WS_SERVER_HOST ?? 'localhost',
+      port: +(process.env.WS_SERVER_PORT ? process.env.WS_SERVER_PORT : 3002),
+      host: process.env.WS_SERVER_HOST
+        ? process.env.WS_SERVER_HOST
+        : 'localhost',
     },
     graphql: {
-      httpPath: process.env.GRAPHQL_HTTP_PATH ?? '/graphql',
-      wsPath: process.env.GRAPHQL_WS_PATH ?? '/graphql',
+      httpPath: process.env.GRAPHQL_HTTP_PATH
+        ? process.env.GRAPHQL_HTTP_PATH
+        : '/graphql',
+      wsPath: process.env.GRAPHQL_WS_PATH
+        ? process.env.GRAPHQL_WS_PATH
+        : '/graphql',
       strict: false,
       viewer: true,
       fillEmptyResponses: true,
@@ -57,16 +67,24 @@ if (require.main === module) {
       graphiql: process.env.NODE_ENV !== 'production',
     },
     mqtt: {
-      url: `${process.env.ALOES_BROKER_URL ?? 'ws://localhost:3000'}`,
-      subPrefix: `aloes-${process.env.ALOES_ID ?? 0}/+/tx/+`,
+      url: `${
+        process.env.ALOES_BROKER_URL
+          ? process.env.ALOES_BROKER_URL
+          : 'ws://localhost:3000'
+      }`,
+      subPrefix: `aloes-${
+        process.env.ALOES_ID ? process.env.ALOES_ID : 0
+      }/+/tx/+`,
       options: {
         keepalive: 60,
         reconnectPeriod: 2000,
         connectTimeout: 2 * 1000,
         clean: true,
-        clientId: `aloes-${process.env.ALOES_ID ?? 0}-graphql`,
-        username: process.env.ALOES_ID ?? '',
-        password: process.env.ALOES_KEY ?? '',
+        clientId: `aloes-${
+          process.env.ALOES_ID ? process.env.ALOES_ID : 0
+        }-graphql`,
+        username: process.env.ALOES_ID ? process.env.ALOES_ID : '',
+        password: process.env.ALOES_KEY ? process.env.ALOES_KEY : '',
       },
     },
   };
